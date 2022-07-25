cbe0f3fbcbe553a55b5fb7c2d9cb75b0900159e9
Phoenix Eliot <phoenixjeliot@gmail.com>
2022-07-25 18:30:46 -0400

    Add example for pulling entire loan history

---
+++ examples/get-history-loans.ts
@@ -0,0 +1,32 @@
+import { createSharkyClient, fetchAllHistoryLoans } from '@sharkyfi/client'
+import { Command } from 'commander'
+import { createProvider } from './lib/createProvider'
+
+// Usage
+// npx ts-node examples/get-history-loans.ts --wallet-path ~/.config/solana/id.json --loan <pubkey>
+
+// If you don't want to use the client library, you can also just request the page directly and combine the paginated results yourself (each page may overlap in the case of duplicate dateOffered fields)
+// https://sharky-git-phoenix-shark-971-expose-history-dat-aeb0a2-sharkyfi.vercel.app/api/loan-history/all?network=mainnet&deployEnvironment=production&before=<nothing or last date from the previous page>
+
+async function main() {
+  // Either use the function directly if you don't need a solana client
+  const historyLoans = await fetchAllHistoryLoans()
+  console.log(`Got ${historyLoans.length} historical loans`)
+
+  // Or use it on the client if you already have one, for consistency
+  const cli = new Command()
+  const options = cli
+    .option('--wallet-path <path>', 'path to private key file')
+    .parse()
+    .opts()
+
+  if (options.walletPath) {
+    const provider = createProvider(options.walletPath)
+    const sharkyClient = createSharkyClient(provider)
+
+    const historyLoans2 = await sharkyClient.fetchAllHistoryLoans()
+    console.log(`Got ${historyLoans2.length} historical loans`)
+  }
+}
+
+main()
